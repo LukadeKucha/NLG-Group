4776c5826483933d30e7ee94c0a86bf7af91980e
Nicolo <lukakuchaidze@gmail.com>
2023-04-20 13:57:22 +0400

    .

--- carousel.js
+++ carousel.js
@@ -6,19 +6,16 @@ const nextBtn = document.querySelector('.carousel-next');
 let currentIndex = 0;
 let slideInterval;
 
-// Move carousel to the next slide
 function nextSlide() {
   currentIndex = (currentIndex + 1) % carouselSlides.length;
   carousel.style.transform = `translateX(-${currentIndex * 50}%)`;
 }
 
-// Move carousel to the previous slide
 function prevSlide() {
   currentIndex = (currentIndex - 1 + carouselSlides.length) % carouselSlides.length;
   carousel.style.transform = `translateX(-${currentIndex * 50}%)`;
 }
 
-// Add event listeners for previous and next buttons
 prevBtn.addEventListener('click', prevSlide);
 nextBtn.addEventListener('click', nextSlide);
 
