3ab5c3679bfec088ef839848d39eb01ae0b905c8
Nicolo <lukakuchaidze@gmail.com>
2023-04-23 15:29:03 +0400

    ..

---
+++ projects/projects.js
@@ -0,0 +1,24 @@
+const carousel = document.querySelector('.carousel');
+const carouselSlides = document.querySelectorAll('.slide');
+const prevBtn = document.querySelector('.carousel-prev');
+const nextBtn = document.querySelector('.carousel-next');
+
+let currentIndex = 0;
+let slideInterval;
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % carouselSlides.length;
+  carousel.style.transform = `translateX(-${currentIndex * 25}%)`;
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + carouselSlides.length) % carouselSlides.length;
+  carousel.style.transform = `translateX(-${currentIndex * 25}%)`;
+}
+
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
+// Set automatic slide interval
+// slideInterval = setInterval(() => {
+//   nextSlide();
+// }, 4000);
