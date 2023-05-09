88f1a4ce4d752b0a2d0d9fd3114d9ce993b6c393
LukadeKucha <lukakuchaidze@mziuri.ge>
2023-05-09 16:36:54 +0400

    .

--- projects/projects.js
+++ projects/projects.js
@@ -28,15 +28,16 @@ const changeSlide = (index) => {
   // Activate the current slide and thumbnail image
   slides[index].classList.add('active');
   thumbnails[thumbnailIndex[index][0]].classList.add('active');
-    const activeThumbnail = thumbnails[thumbnailIndex[index][0]];
-    activeThumbnail.classList.add('active');
-    // activeThumbnail.style.border = '2px solid red'; // add border to the active thumbnail
+  const activeThumbnail = thumbnails[thumbnailIndex[index][0]];
+  activeThumbnail.classList.add('active');
+  // activeThumbnail.style.border = '2px solid red'; // add border to the active thumbnail
   currentSlideIndex = index;
 
   // Activate the corresponding slide button
   slideButtons.forEach((button) => button.classList.remove('active'));
   slideButtons[index].classList.add('active');
 };
+
 // Loop through all thumbnails to add event listener
 thumbnails.forEach((thumbnail) => {
   thumbnail.addEventListener('click', () => {
@@ -72,3 +73,53 @@ slideButtons.forEach((button, index) => {
 // Activate the first slide and thumbnail image
 changeSlide(currentSlideIndex);
 thumbnails[thumbnailIndex[currentSlideIndex][0]].classList.add('active');
+
+// Next and Prev slides
+
+// Add event listeners to the prev and next buttons
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const slidess = document.querySelectorAll('.slide');
+
+let currentIndex = 0;
+
+prevButton.addEventListener('click', () => {
+  // Deactivate all thumbnail images
+  thumbnails.forEach((thumbnail) =>
+    thumbnail.classList.remove('active')
+  );
+
+  slidess[currentIndex].classList.remove('active');
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = slidess.length - 1;
+  }
+  const currentSlide = slidess[currentIndex];
+  currentSlide.classList.add('active');
+  const currentSlideThumbnails = currentSlide.querySelectorAll('.thumbnails img');
+  currentSlideThumbnails[0].classList.add('active');
+});
+
+nextButton.addEventListener('click', () => {
+  // Deactivate all thumbnail images
+  thumbnails.forEach((thumbnail) =>
+    thumbnail.classList.remove('active')
+  );
+
+  slidess[currentIndex].classList.remove('active');
+  currentIndex++;
+  if (currentIndex >= slidess.length) {
+    currentIndex = 0;
+    }
+    // Deactivate all thumbnail images
+    thumbnails.forEach((thumbnail) => thumbnail.classList.remove('active'));
+    
+    // Activate the thumbnail image of the current slide
+    const slideThumbnails = slidess[currentIndex].querySelectorAll('.thumbnails img');
+    const activeThumbnail = slideThumbnails[0];
+    activeThumbnail.classList.add('active');
+    // activeThumbnail.style.border = '2px solid red'; // add border to the active thumbnail
+    
+    // Change the current slide
+    changeSlide(currentIndex);
+    });
